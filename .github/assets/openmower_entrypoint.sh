81c3fe2e64b9a69c624d6b3e2ec8718554eb4fc3
Cedric Hauber <chauber@onewealthplace.com>
2024-05-14 23:13:15 +0200

    switch to isolated build

--- .github/assets/openmower_entrypoint.sh
+++ .github/assets/openmower_entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 # setup ros environment
 source "/opt/ros/$ROS_DISTRO/setup.bash"
-source /opt/open_mower_ros/devel/setup.bash
+source /opt/open_mower_ros/devel_isolated/setup.bash
 source /config/mower_config.sh
 
 # source the hardware specific default environment (default wheel ticks, antenna position etc)
